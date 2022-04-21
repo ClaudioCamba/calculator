f75ebbacacbf5b58ad27a900d11be822af933b1a
Claudio Camba <claudio.a.jose@gmail.com>
2022-04-21 14:34:54 +0100

    Add functions for add, subtract, multiply and divide

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log('start');
+
+// Add function
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(4, 6));
+
+// Subtract function
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(10, 7));
+
+// multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5, 7));
+
+// divide function
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(100, 4));
+
